f386b8a336f62484e1ad4b10cf00dcc02cd23125
Abhijith <abhijithk.sabu@vensure.com>
2024-07-09 16:55:34 +0530

    second

---
+++ example/flutter_lints_vensure_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_lints_vensure/flutter_lints_vensure.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/flutter_lints_vensure_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_lints_vensure/flutter_lints_vensure.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
